4eee1889e13e779b2308a602b1eb2e359ab6cb09
rajharshcr7 <rajharsh.footballer@gmail.com>
2024-02-23 16:41:03 +0530

    February 2024 --solution added

---
+++ 724. Find Pivot Index/README.md
@@ -0,0 +1,9 @@
+## Problem
+[Problem Link](https://leetcode.com/problems/find-pivot-index/)
+
+## Algorithm
+*Coming soon*
+
+## Tags
+ - Array
+ - Prefix Sum
---
+++ 724. Find Pivot Index/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            rightSum += nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+            if (leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
